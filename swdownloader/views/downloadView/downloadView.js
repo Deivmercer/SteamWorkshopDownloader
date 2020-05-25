def7b1a70df7d05ed858978485aa2a7d4e34f0b9
Deivmercer <davemercerdave@gmail.com>
2020-05-25 23:39:25 +0200

    First commit - downloadView and downloadViewCtrl

---
+++ swdownloader/views/downloadView/downloadView.js
@@ -0,0 +1,32 @@
+"use strict";
+
+angular.module("swdownloader.downloadView", ["ngRoute"])
+	.config(["$routeProvider", function($routeProvider) {
+
+		$routeProvider.when("/swdownloader/download", {
+			templateUrl: "/views/downloadView/downloadView.html",
+			controller: "downloadViewCtrl"
+		});
+	}])
+	.controller("downloadViewCtrl", function($scope, $http, userService) {
+
+		$scope.userService = userService;
+		$scope.username = userService.username;
+		$scope.result = "";
+
+		$scope.disable= false;
+		if($scope.userService.permission === 'N') {
+			$scope.disable = true;
+			$scope.result = "Your account has not been activated yet. Contact your server administrator.";
+		}
+
+		$scope.download = function() {
+			$scope.result = "";
+			$http.post("/swdownloader/downloadaction", {"userId": userService.id, "contentId": $scope.contentId})
+				.then(res => $scope.result = res.data)
+				.catch(err => {
+					$scope.result = "Internal server error. Check your browser's console for details.";
+					console.log(err);
+				})
+		}
+	});
