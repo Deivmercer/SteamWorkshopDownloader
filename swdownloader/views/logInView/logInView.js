b791f5597934c22357acc9c3e0f3173d71dfeb84
Deivmercer <davemercerdave@gmail.com>
2020-05-25 23:38:49 +0200

    First commit - logInView and logInViewController

---
+++ swdownloader/views/logInView/logInView.js
@@ -0,0 +1,55 @@
+"use strict";
+
+angular.module("swdownloader.logInView", ["ngRoute"])
+	.config(["$routeProvider", function($routeProvider) {
+
+		$routeProvider.when("/swdownloader/login", {
+			templateUrl: "/views/logInView/logInView.html",
+			controller: "logInViewCtrl"
+		});
+	}])
+	.controller("logInViewCtrl", function($scope, $http, userService) {
+		$scope.userService = userService;
+		$scope.info = "";
+
+		$scope.logIn = function() {
+
+			let requestBody = {
+				"username": $scope.username,
+				"password": $scope.password
+			};
+			$http.post("/swdownloader/loginaction", requestBody)
+				.then(res => {
+					userService.id = res.data.id;
+					userService.username = $scope.username;
+					userService.permission = res.data.permission;
+					window.location.href = "#!/swdownloader";
+				}).catch(err => {
+					if (err.status === 401)
+						$scope.info = err.data;
+					console.log(err);
+				});
+		};
+
+		$scope.signUp = function() {
+
+			let requestBody = {
+				"username": $scope.username,
+				"password": $scope.password
+			};
+			$http.post("/swdownloader/signupaction", requestBody)
+				.then(res => {
+					userService.id = res.data.id;
+					userService.username = $scope.username;
+					userService.permission = res.data.permission;
+					window.location.href = "#!/swdownloader";
+				}).catch(err => {
+					if (err.status === 400 || err.status === 401)
+						$scope.info = err.data;
+					else {
+						$scope.info = "Something went wrong.";
+						console.log(err);
+					}
+				});
+		}
+	});
